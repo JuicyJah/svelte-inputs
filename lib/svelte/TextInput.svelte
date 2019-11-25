dd48061682ce650248200d964029681ecde06b6a
JuicyJah <17132956+JuicyJah@users.noreply.github.com>
2019-11-25 14:22:02 -0500

    Adding Masking (#1)
    * adding masking to inputs
    
    * updating README
    
    - updating README
    - added disable property
    
    * updating ignore files
    
    * 0.4.0
    
    * updating keywords
    
    * 0.4.1

--- lib/svelte/TextInput.svelte
+++ lib/svelte/TextInput.svelte
@@ -1,14 +1,50 @@
 <script>
-  import { createEventDispatcher } from 'svelte'
+  import { createEventDispatcher, onMount } from 'svelte'
+  import { guid, Mask } from '../util/inputhelper.js'
   let dispatch = createEventDispatcher()
 
   export let className = null
   export let value = null
+  let rawvalue = null
   export let placeholder = null
+  export let disabled = false
+  export let mask = {
+    override: false,
+    mask: null
+  }
+  $: _mask = new Mask(mask)
 
-  const change = (value) => dispatch('change', value)
-  const blur = (value) => dispatch('blur', value)
-  const focus = (value) => dispatch('focus', value)
+  const onchange = (value) => dispatch('change', value)
+  const onfocus = (value) => dispatch('focus', value)
+  const onblur = (value) => dispatch('blur', value)
+  const onkeyup = (value) => dispatch('keyup', value)
+
+  function onKeyup(event){
+    if(_mask)
+      _mask.onKeyEvent(event)
+    returnEvent(onkeyup)
+  }
+
+  function returnEvent(func){
+    rawvalue = _mask ? _mask.getDisplayValue() : rawvalue
+    value = _mask ? _mask.getValue() : rawvalue
+    if(func)
+      func(value)
+  }
+
+  let id = null
+  onMount(() => {
+    rawvalue = value
+    if(rawvalue && rawvalue.trim() != ''){
+      focus = true
+    }
+    if(_mask){
+      _mask.setValue(rawvalue)
+      rawvalue = _mask.getDisplayValue()
+      returnEvent()
+    }
+    id = guid()
+  })
 </script>
 <style>
 .svelte-inputs-input{
@@ -18,11 +54,18 @@
   color: inherit;
   display: inline;
 }
+.svelte-inputs-input.disabled{
+  cursor: not-allowed;
+  background-color: #bbb;
+}
 </style>
-<input class="svelte-inputs-input{className ? className : ''}"
-  bind:value={value}
+<input id="svelte-inputs-input-{id}" class="svelte-inputs-input{className ? className : ''}"
+  class:disabled={disabled === true}
+  bind:value={rawvalue}
   placeholder={placeholder}
-  on:focus={() => focus(value)}
-  on:change={() => change(value)}
-  on:blur={() => blur(value)}
+  on:focus={() => returnEvent(onfocus)}
+  on:change={() => returnEvent(onchange)}
+  on:blur={() => returnEvent(onblur)}
+  on:keyup={onKeyup}
+  disabled={disabled === true}
   type="text" />
